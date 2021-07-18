e250d4eb027f53237ad0c0f841f7aab94083ee4e
Gabriel <78141406+l3lackJack@users.noreply.github.com>
2021-07-18 13:58:47 +0200

    seguimos haciendo metodos

--- Metodos/src/metodos/Metodos.java
+++ Metodos/src/metodos/Metodos.java
@@ -24,4 +24,46 @@ public class Metodos {
     public static double conversionReaumur(double a){
         return a*0.8;
     }
+    
+    public static void parImpar(int a){
+        if (a%2==0) {
+            System.out.println("Par");
+        }else{
+            System.out.println("Impar");
+        }
+    }
+    
+    public static void multiplo(int a){
+        if (a%10==0) {
+            System.out.println("Es multiplo");
+        }else{
+            System.out.println("No es multiplo");
+        }
+    }
+    //Para leer un caracter se usa variable =(char)System.in.read
+    public static void mayuscula(char a){
+        if (Character.isUpperCase(a)) {
+            System.out.println("Es una letra mayuscula");
+        }else{
+            System.out.println("No es una letra mayuscula");
+        }
+    }
+    
+    public static void caracteresIguales(char a, char b){
+        if (a==b) {
+            System.out.println("Son iguales");
+        }else{
+            System.out.println("No son iguales");
+        }
+    }
+    //if a>='a' && a<='z'
+    public static void minuscula(char a){
+        if (Character.isLowerCase(a)) {
+            System.out.println("Es minuscula");
+        }else{
+            System.out.println("No es minuscula");
+        }
+    }
+    
+    
 }
